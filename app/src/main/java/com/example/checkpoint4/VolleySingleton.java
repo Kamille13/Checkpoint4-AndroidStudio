94e74fa77891ba2e610b14f2bd6caf0aa064ee48
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-07-16 15:24:30 +0200

    ajout UserSingleton + VolleySingleton

---
+++ app/src/main/java/com/example/checkpoint4/VolleySingleton.java
@@ -0,0 +1,149 @@
+package com.example.checkpoint4;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.checkpoint4.model.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class VolleySingleton {
+
+    private static final String URL = "http://10.0.2.2:8080/";
+
+    private static VolleySingleton instance;
+    private static Context context;
+    private RequestQueue requestQueue;
+
+    private VolleySingleton(Context context) {
+        VolleySingleton.context = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new VolleySingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public void postUser(User user, final Consumer<User> listener) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
+
+        final String requestBody = gson.toJson(user);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                URL+"/users", null, new Response.Listener<JSONObject>() {
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResponse(JSONObject response) {
+                User user = gson.fromJson(response.toString(), User.class);
+                listener.accept(user);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Erreur")
+                        .setMessage("Something went wrong !")
+                        .show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+
+        addToRequestQueue(jsonObjectRequest);
+    }
+    public void signIn(User user, final Consumer<User> listener) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
+        final String requestBody = gson.toJson(user);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                URL+"/users/signIn", null, new Response.Listener<JSONObject>() {
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResponse(JSONObject response) {
+                User user = gson.fromJson(response.toString(), User.class);
+                listener.accept(user);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Erreur")
+                        .setMessage("Votre e-mail ou votre mot de passe n'est pas valide !")
+                        .show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+
+        addToRequestQueue(jsonObjectRequest);
+    }
+}
